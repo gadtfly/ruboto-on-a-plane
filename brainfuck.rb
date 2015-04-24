96e3bdc3eeedf68a0a8108249adf0f6e49be24f7
Adam Louis <adamlouis@gmail.com>
2015-04-23 20:46:14 -0400

    add brainfuck

---
+++ brainfuck.rb
@@ -0,0 +1,48 @@
+program = ",[.-]"
+
+$input = [5].to_enum
+
+language = {
+  '+' => ->{ a[i] += 1 },
+  '-' => ->{ a[i] -= 1 },
+  '>' => ->{ @i += 1 },
+  '<' => ->{ @i -= 1 },
+  '[' => ->{ s << c },
+  ']' => ->{ a[i].zero? ? s.pop : @c = s.last },
+  ',' => ->{ a[i] = gets },
+  '.' => ->{ puts a[i] }
+}
+
+module SpoofedGets
+  def gets
+    $input.next
+  end
+end
+
+class ZeroedArray < Array
+  def [](i)
+    super(i) || 0
+  end
+end
+
+class VM
+  include SpoofedGets
+  attr_accessor :l, :p, :c, :s, :a, :i
+  
+  def initialize(l, p)
+    @l = l
+    @p = p
+    @c = 0
+    @s = []
+    @a = ZeroedArray.new
+    @i = 0
+  end
+
+  def run!
+    instance_exec(&l[p[c]])
+    @c += 1
+    run! if p[c]
+  end
+end
+
+VM.new(language, program).run!
